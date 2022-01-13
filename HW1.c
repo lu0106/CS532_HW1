e1e0b93d8f058e8c2465d3baa2a0fc8c60d993ae
lu0106 <85585301+lu0106@users.noreply.github.com>
2022-01-13 14:14:45 -0500

    Add files via upload

---
+++ HW1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, char* argv[])
+{
+
+    // file open
+    FILE *fp;
+    int number;
+    
+    char filepath[100];
+    printf("Path: ");
+    scanf("%s", filepath);
+    fp = fopen(filepath, "r");
+    
+
+    
+    int count[argc];
+    memset(count, 0, sizeof(count));
+    char *word = malloc(sizeof(char));
+
+    if (fp == NULL)
+		exit(EXIT_FAILURE);
+
+    while(fscanf(fp,"%s",word) >0)
+    {
+        //printf("%s\n", word);
+        for(int i=0;i<argc;i++)
+        {
+            if (strcmp(argv[i], word) == 0)
+            {
+                count[i] += 1;
+            }
+        }
+    }
+
+    for (int i=0;i<argc;i++){
+        printf("%s:   %d\n", argv[i], count[i]);
+    }
+    
+    free(word);
+    fclose(fp);
+    return 0;
+}
